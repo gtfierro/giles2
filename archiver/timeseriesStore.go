5da028cf790c095689a1630b08f637e1c10edda4
Gabe Fierro <gtfierro225@gmail.com>
2015-09-01 16:28:04 -0700

    flesh out timeseries interface

--- archiver/timeseriesStore.go
+++ archiver/timeseriesStore.go
@@ -1,11 +1,21 @@
 package archiver
 
 type TimeseriesStore interface {
-	// add the following SmapMessage to the timeseries database
-	Add(msg *SmapMessage) error
+	AddMessage(msg *SmapMessage) error
+
+	// add the following SmapReading to the timeseries database
+	AddBuffer(*streamBuffer) error
 
 	// list of UUIDs, reference time, units of reference time
 	// Retrieves data before the reference time for the given streams.
 	//TODO: what is the return type here?
 	Prev([]UUID, uint64, UnitOfTime) ([]SmapNumbersResponse, error)
+
+	// list of UUIDs, reference time, units of reference time
+	// Retrieves data after the reference time for the given streams.
+	//TODO: what is the return type here?
+	Next([]UUID, uint64, UnitOfTime) ([]SmapNumbersResponse, error)
+
+	// uuids, start time, end time, unit of time
+	GetData([]UUID, uint64, uint64, UnitOfTime) ([]SmapNumbersResponse, error)
 }

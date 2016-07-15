15ff09c7bdc54eb9db868f9b5fbf419019381958
Gabe Fierro <gtfierro225@gmail.com>
2016-07-15 11:04:42 -0700

    add interactive query

--- clients/bosswave/main.go
+++ clients/bosswave/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
 	"github.com/gtfierro/giles2/clients/bosswave/api"
 	bw "gopkg.in/immesys/bw2bind.v5"
+	"gopkg.in/readline.v1"
 	"os"
+	"os/user"
 )
 
 func doQuery(c *cli.Context) error {
@@ -15,10 +18,58 @@ func doQuery(c *cli.Context) error {
 	return API.Query(c.String("query"))
 }
 
+func doIQuery(c *cli.Context) error {
+	client := bw.ConnectOrExit("")
+	vk := client.SetEntityFileOrExit(c.String("entity"))
+	client.OverrideAutoChainTo(true)
+	API := api.NewAPI(client, vk, c.String("archiver"))
+
+	currentUser, err := user.Current()
+	if err != nil {
+		return err
+	}
+
+	completer := readline.NewPrefixCompleter(
+		readline.PcItem("select",
+			readline.PcItem("data",
+				readline.PcItem("in"),
+				readline.PcItem("before"),
+				readline.PcItem("after"),
+			),
+			readline.PcItem("Metadata/"),
+			readline.PcItem("distinct",
+				readline.PcItem("Metadata/"),
+				readline.PcItem("uuid/"),
+			),
+			readline.PcItem("uuid"),
+		),
+	)
+
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:       "(gquery)>",
+		AutoComplete: completer,
+		HistoryFile:  currentUser.HomeDir + "/.gqueryhist",
+	})
+	if err != nil {
+		return err
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		API.Query(line)
+	}
+	return nil
+}
+
 func main() {
 	app := cli.NewApp()
-	app.Name = "savepoint"
-	app.Version = "0.0.1"
+	app.Name = "gquery"
+	app.Version = "0.0.3"
 
 	app.Commands = []cli.Command{
 		{
@@ -41,6 +92,24 @@ func main() {
 					Name:  "query,q",
 					Value: "",
 					Usage: "Giles query string",
+				},
+			},
+		},
+		{
+			Name:   "iquery",
+			Usage:  "Evaluate query interactively",
+			Action: doIQuery,
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "entity,e",
+					Value:  "",
+					Usage:  "The entity to use",
+					EnvVar: "BW2_DEFAULT_ENTITY",
+				},
+				cli.StringFlag{
+					Name:  "archiver,a",
+					Value: "gabe.ns",
+					Usage: "REQUIRED. The URI you want to archive",
 				},
 			},
 		},
